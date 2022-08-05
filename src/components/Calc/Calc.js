de6f888b2882e54820acb14333a228955f1c48df
mathêe <matheusfc2626@gmail.com>
2022-08-05 12:28:17 -0300

    calculos :p

--- src/components/Calc/Calc.js
+++ src/components/Calc/Calc.js
@@ -1,7 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./styles.module.css"
 
 export default function Calc() {
+  const [resposta, setResposta] = useState()
+  let primeiroNum = Math.floor(Math.random() * 11)
+  let segundoNum = Math.floor(Math.random() * 11)
+  let trueResultado
+
+  const updateResposta = e => {
+    setResposta(e.target.value)
+  }
+
+  const gerarConta = () => {
+    let verificador = Math.floor(Math.random() * 3)
+
+    switch (verificador) {
+      case 0:
+        trueResultado = primeiroNum + segundoNum
+        break;
+      case 1:
+        trueResultado = primeiroNum - segundoNum
+        break;
+      case 2: 
+        trueResultado = primeiroNum * segundoNum
+        break;
+    }
+  }
+
+  const rCerta = () => {
+    
+  }
+  const rErrada = () => {
+
+  }
+
+  const compararResultado = () => {
+    if (resposta === trueResultado) {
+      return rCerta()
+    }
+    rErrada()
+  }
+
   return (
     <div>
       <header>
@@ -13,8 +52,11 @@ export default function Calc() {
       </div>
 
       <div>
-        <input placeholder="RESPOSTA..."/>
-        <button className={styles.buttonCalc}>ATACAR</button>
+        <input 
+          placeholder="RESPOSTA..."
+          onChange={updateResposta}/>
+        <button 
+          className={styles.buttonCalc}>ATACAR</button>
         <button className={styles.buttonCalc}>CURAR</button>
 
       </div>
