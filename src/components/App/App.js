5c8588d6d9c3c3f32c6e0aa2a6d5febaf5ffb8a8
mathêe <91820113+notmathee@users.noreply.github.com>
2022-08-05 18:01:48 -0300

    Add files via upload

---
+++ src/components/App/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { Calc } from "../"
+import styles from "./styles.module.css"
+
+export const App = () => {
+  return <div className={styles.main}><Calc /></div>;
+}
