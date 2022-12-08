373c554359921f7696ac945f91ab6b39e511f851
mathêe <91820113+notmathee@users.noreply.github.com>
2022-12-08 20:37:45 -0300

    Turnos feitos e funcionando 5

--- src/components/Battle/Battle.js
+++ src/components/Battle/Battle.js
@@ -2,27 +2,37 @@ import React, { useEffect, useState } from "react";
 import { Calc } from "../Calc";
 import styles from "./styles.module.css";
 
-export const Battle = () => {
+export const Battle = ({ setModo }) => {
     const [turnoEnemy, setTurnoEnemy] = useState(false)
     const [indicador, setIndicador] = useState('char')
     const [cooldown, setCooldown] = useState('')
+    const [curaCooldown, setCuraCooldown] = useState('')
+    const [curaCounter, setCuraCounter] = useState(0)
 
     useEffect(() => {
         if (turnoEnemy) {
             setIndicador('enem')
             setCooldown('disabled')
+            setCuraCooldown('disabled')
+
+            setTimeout(() => {
+                setIndicador('char')
+                setCooldown('')
+                if (curaCounter <= 2) curaCooldown('') 
+            }, 4500);
         } 
-        setTimeout(() => {
-            setIndicador('char')
-            setCooldown('')
-        }, 4500);
     })
 
     return (
         <div className={styles.mainContainer}>
             <Calc
+                setModo={setModo}
                 cooldown={cooldown}
                 setCooldown={setCooldown}
+                curaCooldown={curaCooldown}
+                setCuraCooldown={setCuraCooldown}
+                curaCounter={curaCounter}
+                setCuraCounter={setCuraCounter}
                 indicador={indicador}
                 turnoEnemy={turnoEnemy}
                 setTurnoEnemy={setTurnoEnemy}
