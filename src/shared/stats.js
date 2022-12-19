26a0b513817058e8ac633cee087dfa036cae1df2
mathêe <matheusfc2626@gmail.com>
2022-12-19 17:36:41 -0300

    esqueci os stats tbm

--- src/shared/stats.js
+++ src/shared/stats.js
@@ -15,20 +15,20 @@ export const enemyStats = {
 export const chronosStats = {
     areiaNome: 'Laços Temporais',
     areiaDano: 20,
-    areiaChronosInicial: 1,
-    areiaChronosTotal: 1,
+    areiaChronosInicial: 3,
+    areiaChronosTotal: 4,
     areiaDescricao: "Ataque com a forma física do Tempo. Finos feixes de areia que parecem vivos envolvem o alvo.",
     areiaEfeito: "O inimigo começa a definhar. Quando finaliza seu turno, recebe dano por 3 rodadas.",
 
     marcaNome: 'Olhar da Passagem',
-    marcaChronosInicial: 1,
-    marcaChronosTotal: 1,
+    marcaChronosInicial: 4,
+    marcaChronosTotal: 4,
     marcaDescricao: "Vislumbre a vida do oponente no Fluxo Temporal e entenda suas fraquezas por uma fração de segundo.",
     marcaEfeito: "Pode apenas ser usado enquanto prepara um ataque. Se acertar, o golpe será crítico.",
 
     escudoNome: 'Vazio Infinito',
-    escudoChronosInicial: 1,
-    escudoChronosTotal: 1,
+    escudoChronosInicial: 5,
+    escudoChronosTotal: 5,
     escudoDescricao: "Uma barreira de infinitude surge ao seu redor para desacelerar quaisquer ataques e perigos. Se desfaz ao tomar um golpe.",
     escudoEfeito: "Recebe metade de dano do ataque seguinte. O inimigo recebe metade do dano que causou. Você não morre com a habilidade ativa.",
 }
